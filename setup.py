2ba89181f729c7d21b28d3f4478f6d715fa1f5cd
hturner1994 <hannah.turner@ninetyone.com>
2023-11-16 08:41:52 +0000

    changing name in set up file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="verysimplemodule", 
+        name="pc-barra", 
         version=VERSION,
         author="Jason Dsouza",
         author_email="<youremail@email.com>",
